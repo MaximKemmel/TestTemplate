3bdd452d3cc07e532324c15ae6506313c85eda68
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2023-10-08 15:25:46 +0300

    Binding ListView

--- TestTemplate/MainWindow.xaml.cs
+++ TestTemplate/MainWindow.xaml.cs
@@ -1,18 +1,52 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
+using TestTemplate.models;
 
 namespace TestTemplate
 {
    public partial class MainWindow : Window
    {
+      List<Contact> contacts { get; set; }
+
       public MainWindow()
       {
          InitializeComponent();
+         LoadContacts();
       }
 
       private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
       {
          DragMove();
+      }
+
+      private void LoadContacts()
+      {
+         contacts = new List<Contact> {
+            new Contact()
+            {
+               ContactName = "Contact 1",
+               ContactType = "Text",
+               Phone = "+00000001",
+               Email = "test1@contact.com"
+            },
+            new Contact()
+            {
+               ContactName = "Contact 2",
+               ContactType = "Text",
+               Phone = "+00000002",
+               Email = "test2@contact.com"
+            },
+            new Contact()
+            {
+               ContactName = "Contact 3",
+               ContactType = "Text",
+               Phone = "+00000003",
+               Email = "test3@contact.com"
+            }
+         };
+         this.contactsList.ItemsSource = contacts;
       }
    }
 }
---
+++ TestTemplate/models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTemplate.models
+{
+   public class Contact
+   {
+      public string ContactName { get; set; }
+      public string ContactType { get; set; }
+      public string Phone { get; set; }
+      public string Email { get; set; }
+   }
+}
